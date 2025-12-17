7b5e8a6cef89ac545201c6f09d799780b1b1a34d
sinap55 <parvanehs@limestone.on.ca>
2025-12-17 12:42:15 -0500

    fixed bug for goodboy

--- WelcomeView.java
+++ WelcomeView.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 class WelcomeView {
     WelcomeView() {
         int gabe = 5;
         System.out.println("Gabe is a sigma boy");
-        Scanner sc = new Scanner;
-        System.out.println("Is this true")
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Is this true");
         String answer = sc.nextLine();
         if (answer.equals("Yes")) {
             System.out.println("Goodboy");
