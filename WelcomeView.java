c0b504f6a0ac32c8b9acac7354d225b1bd6639a4
sinap55 <parvanehs@limestone.on.ca>
2025-12-17 12:41:03 -0500

    Created goodboy algorithms

--- WelcomeView.java
+++ WelcomeView.java
@@ -1,6 +1,13 @@
 class WelcomeView {
-    WelcomeView () {
+    WelcomeView() {
         int gabe = 5;
-    System.out.println("Gabe is a sigma boy"); 
+        System.out.println("Gabe is a sigma boy");
+        Scanner sc = new Scanner;
+        System.out.println("Is this true")
+        String answer = sc.nextLine();
+        if (answer.equals("Yes")) {
+            System.out.println("Goodboy");
+        }
+
     }
 }
